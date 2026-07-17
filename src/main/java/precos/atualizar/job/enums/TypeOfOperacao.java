f2bcb692809ef4b1952c54c3380c0746c44e09e4
Vitória Bentes <73360746+vitoriabentes@users.noreply.github.com>
2026-07-17 10:56:58 -0300

    Incluindo leitura JSON para ENUM tipo de operação

--- src/main/java/precos/atualizar/job/enums/TypeOfOperacao.java
+++ src/main/java/precos/atualizar/job/enums/TypeOfOperacao.java
@@ -1,6 +1,30 @@
 package precos.atualizar.job.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TypeOfOperacao {
-    COMPRA,
-    VENDA
+    COMPRA("COMPRA"),
+    VENDA("VENDA");
+
+    private String value;
+
+    TypeOfOperacao(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static TypeOfOperacao fromValue(String value) {
+        for (TypeOfOperacao type : TypeOfOperacao.values()) {
+            if (type.value.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Tipo de operação inválida: " + value);
+    }
 }
